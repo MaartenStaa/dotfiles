4050fc356ba848afb42b4549c406807ce6188fd9
Maarten Staa <maarten.staa@brightalley.nl>
2021-08-06 09:45:45 +0200

    Fish: ctrl-f for forward and use nvim as $EDITOR.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,8 @@ set -U fish_user_paths ~/.cargo/bin $fish_user_paths
 
 # Keybindings
 bind -k nul -M insert end-of-line
+bind \cF -M insert nextd-or-forward-word
+
+# Environment
+export EDITOR=/usr/local/bin/nvim
+
