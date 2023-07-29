731189c40bfc7550814ddf9ff1c52901ef7d63ec
Maarten Staa <maartens@spotify.com>
2023-07-29 23:44:17 +0200

    feat: fzf file finding, but faster

--- .config/nvim/lua/navigation.lua
+++ .config/nvim/lua/navigation.lua
@@ -1,19 +1,29 @@
 local fzf = require('fzf-lua')
-vim.keymap.set({ 'n', 'v', 'o' }, '<C-p>', fzf.files)
-vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>ff', fzf.files)
+
+local function fzf_files()
+  fzf.fzf_exec('fd --type f --strip-cwd-prefix | ~/.dotfiles/bin/file-web-devicon', {
+    actions = fzf.defaults.actions.files,
+    previewer = 'builtin',
+  })
+end
+
+vim.keymap.set({ 'n', 'v', 'o' }, '<C-p>', fzf_files, { desc = 'Find files' })
+vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>ff', fzf_files, { desc = 'Find files' })
 vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>fg', function()
   fzf.live_grep({ cmd = "git grep --line-number --column --ignore-case --color=always --untracked" })
-end)
-vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>fb', fzf.buffers)
-vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>fd', function() fzf.files({ cwd = "~/.config/nvim" }) end)
-vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>gd', function() fzf.lsp_definitions({ jump_to_single_result = true }) end)
-vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>gl', fzf.diagnostics_document)
-vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>gr', fzf.lsp_references)
-vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>gi', fzf.lsp_implementations)
-vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>ga', fzf.builtin)
-vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>gs', fzf.lsp_document_symbols)
-vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>fs', fzf.lsp_live_workspace_symbols)
-
+end, { desc = 'Find in files' })
+vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>fb', fzf.buffers, { desc = 'Find open buffers' })
+vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>fd', function() fzf.files({ cwd = "~/.config/nvim" }) end, { desc = 'Find files in nvim config' })
+vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>gl', fzf.diagnostics_document, { desc = 'Find diagnostics in the current document' })
+vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>ga', fzf.builtin, { desc = 'Go to anything' })
+vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>gs', fzf.lsp_document_symbols, { desc = 'Find symbols in the current document' })
+vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>fs', fzf.lsp_live_workspace_symbols, { desc = 'Find symbols in the current workspace' })
+vim.keymap.set({ 'n', 'v', 'o' }, 'gd', function() fzf.lsp_definitions({ jump_to_single_result = true }) end, { desc = 'Go to definition' })
+vim.keymap.set({ 'n', 'v', 'o' }, 'gr', fzf.lsp_references, { desc = 'Find references' })
+vim.keymap.set({ 'n', 'v', 'o' }, 'gi', fzf.lsp_implementations, { desc = 'Find implementations' })
+vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>gp', fzf.resume, { desc = 'Resume last FZF command' })
+vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>gh', fzf.help_tags, { desc = 'Find help tags' })
+vim.keymap.set({ 'n', 'v', 'o' }, '<Leader>/', fzf.grep_curbuf, { desc = 'Find in current buffer' })
 
 -- telescope.load_extension('refactoring')
 --vim.keymap.set(
