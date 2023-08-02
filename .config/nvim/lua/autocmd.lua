03c8d46066f2d2f21508986889b4f88c5d184d25
Maarten Staa <maartens@spotify.com>
2023-08-02 08:39:26 +0200

    fix: add autocmds to group
    also automatically open trouble.nvim when opening quickfix list

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -8,19 +8,76 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 
 -- Fix blade filetype not being set correctly.
 vim.api.nvim_create_autocmd('BufRead,BufNewFile', {
+  group = autosourcing,
   pattern = '*.blade.php',
   command = 'set filetype=blade'
 })
 
 -- Set tabbing based on file type
-vim.api.nvim_create_autocmd('FileType', { pattern = '*.blade', command = 'setlocal ts=2 sts=2 sw=2 expandtab' })
-vim.api.nvim_create_autocmd('FileType', { pattern = '*.css', command = 'setlocal ts=4 sts=4 sw=4 expandtab' })
-vim.api.nvim_create_autocmd('FileType', { pattern = '*.html', command = 'setlocal ts=2 sts=2 sw=2 expandtab' })
-vim.api.nvim_create_autocmd('FileType', { pattern = '*.javascript', command = 'setlocal ts=2 sts=2 sw=2 expandtab' })
-vim.api.nvim_create_autocmd('FileType', { pattern = '*.lua', command = 'setlocal ts=2 sts=2 sw=2 expandtab' })
-vim.api.nvim_create_autocmd('FileType', { pattern = '*.php', command = 'setlocal ts=4 sts=4 sw=4 expandtab' })
-vim.api.nvim_create_autocmd('FileType', { pattern = '*.scss', command = 'setlocal ts=4 sts=4 sw=4 expandtab' })
-vim.api.nvim_create_autocmd('FileType', { pattern = '*.vim', command = 'setlocal ts=4 sts=4 sw=4 expandtab' })
+vim.api.nvim_create_autocmd('FileType', {
+  group = autosourcing,
+  pattern = '*.blade',
+  command = 'setlocal ts=2 sts=2 sw=2 expandtab' }
+)
+vim.api.nvim_create_autocmd('FileType', {
+  group = autosourcing,
+  pattern = '*.css',
+  command = 'setlocal ts=4 sts=4 sw=4 expandtab' }
+)
+vim.api.nvim_create_autocmd('FileType', {
+  group = autosourcing,
+  pattern = '*.html',
+  command = 'setlocal ts=2 sts=2 sw=2 expandtab' }
+)
+vim.api.nvim_create_autocmd('FileType', {
+  group = autosourcing,
+  pattern = '*.javascript',
+  command = 'setlocal ts=2 sts=2 sw=2 expandtab' }
+)
+vim.api.nvim_create_autocmd('FileType', {
+  group = autosourcing,
+  pattern = '*.lua',
+  command = 'setlocal ts=2 sts=2 sw=2 expandtab' }
+)
+vim.api.nvim_create_autocmd('FileType', {
+  group = autosourcing,
+  pattern = '*.md',
+  command = 'setlocal textwidth=80' }
+)
+vim.api.nvim_create_autocmd('FileType', {
+  group = autosourcing,
+  pattern = '*.php',
+  command = 'setlocal ts=4 sts=4 sw=4 expandtab' }
+)
+vim.api.nvim_create_autocmd('FileType', {
+  group = autosourcing,
+  pattern = '*.scss',
+  command = 'setlocal ts=4 sts=4 sw=4 expandtab' }
+)
+vim.api.nvim_create_autocmd('FileType', {
+  group = autosourcing,
+  pattern = '*.vim',
+  command = 'setlocal ts=4 sts=4 sw=4 expandtab' }
+)
 
 -- Never hide git commit messages and such
-vim.api.nvim_create_autocmd('FileType', { pattern = 'gitcommit,gitrebase,gitconfig,diff', command = 'set bufhidden=delete' })
+vim.api.nvim_create_autocmd('FileType', {
+  group = autosourcing,
+  pattern = 'gitcommit,gitrebase,gitconfig,diff',
+  command = 'set bufhidden=delete',
+})
+
+-- Open trouble window when quickfix window is opened
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  group = autosourcing,
+  pattern = 'quickfix',
+  callback = function()
+    local ok, trouble = pcall(require, 'trouble')
+    if ok then
+      vim.defer_fn(function()
+        vim.cmd('cclose')
+        trouble.open('quickfix')
+      end, 0)
+    end
+  end,
+})
