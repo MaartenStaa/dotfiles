0fba61865103f05c090fe61d2a5968f0583c2ded
Maarten Staa <maartens@spotify.com>
2023-08-25 16:21:14 +0200

    chore: remove outdated vim-plug in setup

--- links.sh
+++ links.sh
@@ -1,16 +1,12 @@
 #!/bin/zsh
 
-echo "Downloading Vim Plug"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 echo "Linking configuration files..."
 
 link () {
   SOURCE=~/.dotfiles/$1
   TARGET=~/$1
 
-  if [ ! -h $TARGET ]; then
+  if [ ! -L $TARGET ]; then
     ln -s $SOURCE $TARGET
   fi
 }
@@ -24,4 +20,3 @@ link ".config/karabiner"
 link ".config/kitty"
 link ".config/omf"
 link ".config/nvim"
-link ".vim"
