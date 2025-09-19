5c8e5424231b5b4a5c1135ba2a8bd298f2fc7add
Maarten Staa <maartens@spotify.com>
2025-09-19 14:07:01 +0200

    Enable watch auth for sudo, and reattach for tmux

--- machines/shared/darwin.nix
+++ machines/shared/darwin.nix
@@ -61,8 +61,10 @@
   };
 
   # TODO: Add system defaults
-  security = {
-    pam.services.sudo_local.touchIdAuth = true;
+  security.pam.services.sudo_local = {
+    reattach = true;
+    touchIdAuth = true;
+    watchIdAuth = true;
   };
   system.primaryUser = username;
   system.defaults = {
