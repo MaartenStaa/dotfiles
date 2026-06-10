8cc5d28bc8c6e3b82a7cb8229fa2a603abeb8f16
Maarten Staa <maartens@spotify.com>
2026-06-10 09:51:12 +0200

    feat(home-manager): enable nix-index and disable nixpkgs release check
    Suppress the release version mismatch warning when using unstable
    nixpkgs with a stable home-manager state version, and enable
    nix-index for command-not-found lookups.

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -4,9 +4,10 @@
     inherit username;
     homeDirectory = "/Users/${username}";
     stateVersion = "24.05";
+    enableNixpkgsReleaseCheck = false;
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  # programs.nix-index.enable = true;
+  programs.nix-index.enable = true;
 }
