68e93dc8836056d8a043920f4e61b329c21a1034
Maarten Staa <maartens@spotify.com>
2026-07-27 09:25:55 +0200

    fix(fish): incorrect added paths

--- modules/home-manager/fish/config.fish
+++ modules/home-manager/fish/config.fish
@@ -11,11 +11,9 @@ bind ctrl-f -M insert nextd-or-forward-word
 # Set $PATH variable.
 fish_add_path -m /run/current-system/sw/bin
 fish_add_path -p ~/.nix-profile/bin /nix/var/nix/profiles/default/bin /etc/profiles/per-user/maartens/bin
-fish_add_path $USER/.cargo/bin
+fish_add_path $HOME/.cargo/bin
 fish_add_path /Applications/kitty.app/Contents/MacOS/
-fish_add_path $USER/.yarn/bin
-fish_add_path $USER/.local/bin
-fish_add_path $USER/.fig/bin
+fish_add_path $HOME/.local/bin
 fish_add_path /opt/homebrew/bin
 
 if test -f ~/.config/fish/local.fish
