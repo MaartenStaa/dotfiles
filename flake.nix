feb1677369e01bfff5c2f6ec0e193b10d100c6f8
Maarten Staa <maartens@spotify.com>
2026-07-30 13:38:58 +0200

    refactor: nest home-manager configuration as one object

--- flake.nix
+++ flake.nix
@@ -333,34 +333,37 @@
 
               home-manager.nixosModules.home-manager
               {
-                home-manager.useGlobalPkgs = false;
-                home-manager.useUserPackages = true;
-                home-manager.extraSpecialArgs = {
-                  inherit inputs;
-                  inherit (args) arch username email;
-                  system = arch;
+                home-manager = {
+                  useGlobalPkgs = false;
+                  useUserPackages = true;
+                  extraSpecialArgs = {
+                    inherit inputs;
+                    inherit (args) arch username email;
+                    system = arch;
+                  };
+
+                  users.${username}.imports = [
+                    catppuccin.homeModules.catppuccin
+
+                    ./modules/shared/config.nix
+
+                    ./modules/home-manager/catppuccin.nix
+                    ./modules/home-manager/development.nix
+                    ./modules/home-manager/fd
+                    ./modules/home-manager/fzf.nix
+                    ./modules/home-manager/ghostty
+                    ./modules/home-manager/git
+                    ./modules/home-manager/herdr
+                    ./modules/home-manager/home.nix
+                    ./modules/home-manager/jj
+                    ./modules/home-manager/linux.nix
+                    ./modules/home-manager/neovim
+                    ./modules/home-manager/pkgs.nix
+                    ./modules/home-manager/python.nix
+                    ./modules/home-manager/shell.nix
+                    ./modules/home-manager/tmux
+                  ];
                 };
-                home-manager.users.${username}.imports = [
-                  catppuccin.homeModules.catppuccin
-
-                  ./modules/shared/config.nix
-
-                  ./modules/home-manager/catppuccin.nix
-                  ./modules/home-manager/development.nix
-                  ./modules/home-manager/fd
-                  ./modules/home-manager/fzf.nix
-                  ./modules/home-manager/ghostty
-                  ./modules/home-manager/git
-                  ./modules/home-manager/herdr
-                  ./modules/home-manager/home.nix
-                  ./modules/home-manager/jj
-                  ./modules/home-manager/linux.nix
-                  ./modules/home-manager/neovim
-                  ./modules/home-manager/pkgs.nix
-                  ./modules/home-manager/python.nix
-                  ./modules/home-manager/shell.nix
-                  ./modules/home-manager/tmux
-                ];
               }
             ];
           };
