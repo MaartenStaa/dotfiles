af758e458aa9c262cfecd696e032cbdbf04c1768
Maarten Staa <maarten@staa.dev>
2026-07-30 21:57:59 +0200

    chore: update nixpkgs-stable
    This includes a fix for the Firefox font issue, removing the need for a
    workaround.

--- machines/desktop-pc/display.nix
+++ machines/desktop-pc/display.nix
@@ -19,12 +19,4 @@
     adwaita-icon-theme
     gnome-themes-extra
   ];
-
-  # Workaround for missing fonts in Firefox
-  # See: https://github.com/nixos/nixpkgs/issues/546204
-  environment.sessionVariables.XDG_DATA_DIRS = [
-    "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
-    "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
-  ];
-
 }
