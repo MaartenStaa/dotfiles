fa843c1bbfb5814922b84e1a2381cfece6b573fb
Maarten Staa <maarten@staa.dev>
2026-07-31 13:38:35 +0200

    fix: add path entry in fish for `/run/wrappers/bin`

--- modules/home-manager/shared/shell/fish/config.fish
+++ modules/home-manager/shared/shell/fish/config.fish
@@ -10,11 +10,15 @@ bind ctrl-f -M insert nextd-or-forward-word
 
 # Set $PATH variable.
 fish_add_path -m /run/current-system/sw/bin
-fish_add_path -p ~/.nix-profile/bin /nix/var/nix/profiles/default/bin /etc/profiles/per-user/maartens/bin
+fish_add_path ~/.nix-profile/bin /nix/var/nix/profiles/default/bin /etc/profiles/per-user/maartens/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path /Applications/kitty.app/Contents/MacOS/
 fish_add_path $HOME/.local/bin
 fish_add_path /opt/homebrew/bin
+
+if test -d /run/wrappers/bin
+    fish_add_path -m /run/wrappers/bin
+end
 
 if test -f ~/.config/fish/local.fish
     source ~/.config/fish/local.fish
